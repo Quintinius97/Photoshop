9d3613bb4a89e5b09b57e6f9835f94fa8934ed85
Tom <Tom@Quintinius.mshome.net>
2014-12-03 14:03:38 +0100

    1. Commit - Grundgerüst

---
+++ src/infoprojektbildmanipulation/Effects.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infoprojektbildmanipulation;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Tom
+ */
+public class Effects {
+    private BufferedImage image = null;
+    
+    public Effects(BufferedImage i) {
+        image = i;
+    }
+    
+    public BufferedImage Verwirbeln() {
+        return null;
+    }
+    
+    public BufferedImage Wellen() {
+        return null;
+    }
+    
+    public BufferedImage Einrollen() {
+        return null;
+    }
+    
+    public BufferedImage Leinwand() {
+        return null;
+    }
+    
+    public BufferedImage Oelbild() {
+        return null;
+    }
+    
+    public BufferedImage Kaffee() {
+        return null;
+    }
+    
+    public BufferedImage Puzzle() {
+        return null;
+    }
+    
+    public BufferedImage Comic() {
+        return null;
+    }
+}
---
+++ src/infoprojektbildmanipulation/InfoProjektBildManipulation.java
@@ -0,0 +1,20 @@
+package infoprojektbildmanipulation;
+
+/**
+ *
+ * @author Nicolai Müller
+ * @author Tom Richter
+ * 
+ */
+public class InfoProjektBildManipulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hauptfenster f= new Hauptfenster();
+        f.setVisible(true);
+        f.setSize(800, 600);
+    }
+    
+}
